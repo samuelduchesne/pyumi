eb7f4fbc08edd7c706e56f1ee0458af3ad48b47a
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-02-24 14:37:57 -0500

    Setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from os import path
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 here = os.getcwd()
 
@@ -9,15 +9,21 @@ with open(path.join(here, "requirements.txt")) as f:
     requirements_lines = f.readlines()
 install_requires = [r.strip() for r in requirements_lines if "git+" not in r]
 
+with open(path.join(here, "requirements-dev.txt")) as f:
+    requirements_lines = f.readlines()
+dev_requires = [r.strip() for r in requirements_lines if "git+" not in r]
+
 setup(
     name="pyumi",
     version="1.0",
-    packages=["pyumi"],
+    packages=find_packages(exclude=["tests"]),
     url="",
     license="MIT",
     author="Samuel Letellier-Duchesne",
     author_email="samueld@mit.edu",
     description="Create and edit umi projects",
     python_requires="==3.7.*",
-    install_requires=install_requires + ["epw @ https://github.com/building-energy/epw/archive/master.zip"],
+    install_requires=install_requires
+    + ["epw @ https://github.com/building-energy/epw/archive/master.zip"],
+    extras_require={"dev": dev_requires},
 )
