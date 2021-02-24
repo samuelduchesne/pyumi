a329764d272ac3f94b7cddde9c8242c99d143f9b
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-02-24 18:24:13 -0500

    Uses cms to set version + fixes to setup.py

--- setup.py
+++ setup.py
@@ -15,14 +15,31 @@ dev_requires = [r.strip() for r in requirements_lines if "git+" not in r]
 
 setup(
     name="pyumi",
-    version="1.0",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     packages=find_packages(exclude=["tests"]),
-    url="",
+    url="https://github.com/samuelduchesne/pyumi",
     license="MIT",
     author="Samuel Letellier-Duchesne",
     author_email="samueld@mit.edu",
     description="Create and edit umi projects",
-    python_requires="==3.7.*",
+    python_requires=">=3.6",
     install_requires=install_requires,
     extras_require={"dev": dev_requires},
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Science/Research",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
