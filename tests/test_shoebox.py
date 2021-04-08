f25e10754bcfc9f17563366279ff1c737d663821
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-04-08 16:20:50 -0400

    set internal mass based on template

--- tests/test_shoebox.py
+++ tests/test_shoebox.py
@@ -2,6 +2,7 @@ import uuid
 
 import pytest
 from archetypal import UmiTemplateLibrary
+from shapely.geometry import Polygon, LineString
 
 from pyumi.shoeboxer import ShoeBox
 
@@ -21,13 +22,21 @@ class TestShoebox:
 
     def test_from_template(self, building_template, template):
         name = "test.idf"
-        sb = ShoeBox.from_template(building_template,
-                                   ddy_file="tests/CAN_PQ_Montreal.Intl.AP.716270_CWEC.ddy",
-                                   name=name)
+        sb = ShoeBox.from_template(
+            building_template,
+            ddy_file="tests/CAN_PQ_Montreal.Intl.AP.716270_CWEC.ddy",
+            name=name,
+        )
         sb.saveas(sb.name)
+        sb.outputs.add_dxf().apply()
         sb.simulate(
             epw="tests/USA_MA_Boston-Logan.Intl.AP.725090_TMY3.epw",
             expandobjects=True,
+            design_day=False,
             annual=True,
             keep_data_err=True,
         )
+
+        sb.view_model()
+        sb.meters.OutputMeter.Heating__DistrictHeating.values().plot2d()
+        sb.open_htm()
